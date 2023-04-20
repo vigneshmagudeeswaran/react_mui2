03c949676ecb291c9e7c9ccf19c5ce0c9d6b8675
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-21 02:32:21 +0530

    RadioButton

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import MuiTypography from "./components/MuiTypography";
 import MuiButton from "./components/MuiButton";
 import MuiTextField from "./components/MuiTextField";
 import MuiSelect from "./components/MuiSelect";
+import MuiRadioButton from "./components/MuiRadioButton";
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
       {/* <MuiTextField /> */}
-      <MuiSelect/>
+      {/* <MuiSelect /> */}
+      <MuiRadioButton />
     </div>
   );
 }
