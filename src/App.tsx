90d14dfa692ed5e48446b95f6e23b2421dc67221
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-21 01:39:04 +0530

    Select

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import MuiTypography from "./components/MuiTypography";
 import MuiButton from "./components/MuiButton";
 import MuiTextField from "./components/MuiTextField";
+import MuiSelect from "./components/MuiSelect";
 function App() {
   return (
     <div className="App">
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
-      <MuiTextField />
+      {/* <MuiTextField /> */}
+      <MuiSelect/>
     </div>
   );
 }
---
+++ src/components/MuiSelect.tsx
@@ -0,0 +1,34 @@
+import { Box, TextField, MenuItem } from "@mui/material";
+import { useState } from "react";
+export const MuiSelect = () => {
+  const [countries, setCountries] = useState<string[]>([]);
+  console.log({ countries });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setCountries(typeof value === "string" ? value.split(",") : value);
+  };
+  return (
+    <Box width="250px">
+      <TextField
+        label="select country"
+        select
+        value={countries}
+        onChange={handleChange}
+        fullWidth
+        SelectProps={{
+          multiple: true,
+        }}
+        size="small"
+        color="secondary"
+        helperText="Please select your country"
+        error
+      >
+        <MenuItem value="IN">India</MenuItem>
+        <MenuItem value="US">USA</MenuItem>
+        <MenuItem value="AU">Austraila</MenuItem>
+      </TextField>
+    </Box>
+  );
+};
+
+export default MuiSelect;
